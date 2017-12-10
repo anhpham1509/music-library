64f8533b537ea927df38ed3623b863162663250b
Anh Pham <duyanhpham1509@gmail.com>
2017-12-11 01:59:34 +0200

    feat: actions constants.

---
+++ frontend/constants/song.ts
@@ -0,0 +1,8 @@
+export const songConstants = {
+    GET_SONGS: 'GET_SONGS',
+    GET_SONGS_FAILURE: 'GET_SONGS_FAILURE',
+
+    ADD_SONG: 'ADD_SONG',
+    UPDATE_SONG: 'UPDATE_SONG',
+    REMOVE_SONG: 'REMOVE_SONG'
+};
